a6d5d0a9db606d2b91c2bbd996f66ff1429b0dc5
Alex Quintero <alex.quintero@gmail.com>
2016-06-24 15:45:10 -0400

    added new tools to extract gis data into images and generate kfold test/training sets to input into overfeat

---
+++ models/overfeat.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 22 18:59:57 2016
+
+@author: alex
+"""
+
+from klearn_theano.feature_extraction import OverfeatTransformer
+
+def main():
+    OverfeatTransformer()
+
+if __name__ == "__main__":
+    main()
